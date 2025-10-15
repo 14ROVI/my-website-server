6f88e7a373fd2acc53bc44629b16081121751a22
14ROVI <29734170+14ROVI@users.noreply.github.com>
2025-10-15 20:26:33 +0100

    make it more rusty!

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ use rocket::{
 
 use sqlx::{Pool, Sqlite, SqlitePool};
 
-use select::document::Document;
 use select::predicate::{Attr, Class, Name, Predicate};
+use select::{document::Document, node::Node};
 
 use std::{
     collections::HashMap,
@@ -247,63 +247,66 @@ async fn get_films(state: &State<Arc<Mutex<LetterboxdScrape>>>) -> Json<Vec<Film
         return Json::from(state.last_response.clone());
     }
 
-    let mut films = vec![];
-
-    if let Ok(req) = reqwest::get("https://letterboxd.com/14rovi/films/by/date/size/large/").await {
-        if let Ok(text) = req.text().await {
-            let document = Document::from(text.as_str());
-            for film_node in
-                document.find(Name("div").and(Class("poster-grid")).descendant(Name("li")))
-            {
-                let (Some(name), Some(rating), Some(div_node), Some(watched_at)) = (
-                    film_node
-                        .find(Name("img"))
-                        .next()
-                        .and_then(|n| n.attr("alt"))
-                        .map(|s| s.to_string()),
-                    film_node
-                        .find(Name("span").and(Class("rating")))
-                        .next()
-                        .and_then(|n| n.attr("class"))
-                        .and_then(|c| c.split("-").last())
-                        .and_then(|r| r.parse::<u32>().ok()),
-                    film_node
-                        .find(Attr("data-component-class", "LazyPoster"))
-                        .next(),
-                    film_node
-                        .find(Name("time"))
-                        .next()
-                        .and_then(|n| n.attr("datetime"))
-                        .map(|dt| dt.to_string()),
-                ) else {
-                    continue;
-                };
-
-                let Some(data_item_link) = div_node.attr("data-item-link") else {
-                    continue;
-                };
-
-                films.push(FilmData {
-                    name,
-                    rating,
-                    watched_at,
-                    poster_url: data_item_link.to_string(),
-                });
-            }
-        }
-    }
-
-    let futures = films
-        .iter_mut()
-        .map(|film| set_poster_url(film))
-        .collect::<Vec<_>>();
-
-    join_all(futures).await;
+    let films = get_letterboxd_films().await.unwrap_or_default();
 
     state.last_hit_at = now;
     state.last_response = films.clone();
 
     Json::from(films)
+}
+
+async fn get_letterboxd_films() -> Result<Vec<FilmData>, Box<dyn std::error::Error>> {
+    let mut films = {
+        let html = reqwest::get("https://letterboxd.com/14rovi/films/by/date/size/large/")
+            .await?
+            .text()
+            .await?;
+
+        Document::from(html.as_str())
+            .find(Name("div").and(Class("poster-grid")).descendant(Name("li")))
+            .flat_map(parse_letterboxd_poster)
+            .collect::<Vec<_>>()
+    };
+
+    let futures = films.iter_mut().map(|film| set_poster_url(film));
+
+    join_all(futures).await;
+
+    return Ok(films);
+}
+
+fn parse_letterboxd_poster(film_node: Node) -> Option<FilmData> {
+    let name = film_node
+        .find(Name("img"))
+        .next()
+        .and_then(|n| n.attr("alt"))
+        .map(|s| s.to_string())?;
+
+    let rating = film_node
+        .find(Name("span").and(Class("rating")))
+        .next()
+        .and_then(|n| n.attr("class"))
+        .and_then(|c| c.split("-").last())
+        .and_then(|r| r.parse::<u32>().ok())?;
+
+    let watched_at = film_node
+        .find(Name("time"))
+        .next()
+        .and_then(|n| n.attr("datetime"))
+        .map(|dt| dt.to_string())?;
+
+    let poster_url = film_node
+        .find(Attr("data-component-class", "LazyPoster"))
+        .next()
+        .and_then(|n| n.attr("data-item-link"))
+        .map(|s| s.to_string())?;
+
+    Some(FilmData {
+        name,
+        rating,
+        watched_at,
+        poster_url,
+    })
 }
 
 async fn set_poster_url(film: &mut FilmData) {
